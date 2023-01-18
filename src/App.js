e9a0cb481fb3fa2b5c75f9f2ffd2834cfa2dbc47
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-01-18 18:34:28 +0100

    completing boilerplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from 'react';
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+import {CTA, Brand, Navbar} from './components';
 const App =()=> {
     return (
-        <div>
-            <h1>GPT-3</h1>
+        <div className='App'>
+            <div className='gradient_bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './whatGPT3.css';
+
+const whatGPT3 = () => {
+  return (
+    <div>whatGPT3</div>
+  )
+}
+
+export default whatGPT3
