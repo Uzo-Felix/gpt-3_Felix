c32a3805f49104fbc9e2daf8ba6823023c20ce6e
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-01-22 20:57:21 +0100

    feat:header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,30 +14,30 @@ const Menu =() => (
 )
 
 const Navbar = () => {
-  const {toggleMenu, setToggleMenu} = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div className="gpt3_navbar">
-      <div className= 'gpt3_navbar-links'>
-        <div className="gpt3_navbar-links_logo">
+    <div className="gpt3__navbar">
+      <div className= 'gpt3__navbar-links'>
+        <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
-        <div className="gpt3_navbar-links_container">
+        <div className="gpt3__navbar-links_container">
           <Menu />
         </div>
-        <div className="gpt3_navbar-sign">
+        <div className="gpt3__navbar-sign">
           <p>Sign in</p>
           <button type="button">Sign up</button>
         </div>
-        <div className="gpt3_navbar-menu"> 
+        <div className="gpt3__navbar-menu"> 
         {toggleMenu
         ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
-        :  <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
         }
         {toggleMenu && (
-          <div className='gpt3_navbar-menu_container scale-up-center'>
-            <div className="gpt3_navbar-menu_container-links">
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className="gpt3__navbar-menu_container-links">
               <Menu /> 
-              <div className="gpt3_navbar-menu_container-links-sign">
+              <div className="gpt3__navbar-menu_container-links-sign">
           <p>Sign in</p>
           <button type="button">Sign up</button>
         </div>
