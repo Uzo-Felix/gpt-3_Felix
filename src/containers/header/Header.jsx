6e809c370c4703aee13a75d0b3662534e4f2eb3d
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-01-23 17:06:06 +0100

    feat: adding media query for all devices

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,9 +8,14 @@ const Header = () => {
     <div className='gpt3__header section__padding' id='home'>
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>
-          Let's Build Something amazing with GPT-3 OpenAI
+          Let's Build 
+          Something amazing 
+          with GPT-3 OpenAI
         </h1>
-        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <p>Yet bed any for travelling assistance indulgence unpleasing.
+          Not thoughts all exercise blessing. Indulgence way everything 
+          joy alteration boisterous the attachment. Party we years to 
+          order allow asked of.</p>
         <div className='gpt3__header-content__input'>
           <input type='email' placeholder='Your Email Address' />
           <button type='button'>Get started</button>
@@ -19,10 +24,10 @@ const Header = () => {
           <img src={people} alt='people'/>
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className='gpt3__header-image'>
+      </div>
+      <div className='gpt3__header-image'>
           <img src={ai} alt='ai'/>
         </div>
-      </div>
     </div>
   )
 }
