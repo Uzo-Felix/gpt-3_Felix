367b4658bf6d184d24364eb6d9f7a14d243cd83d
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-01-24 18:26:56 +0100

    feat:component setup

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './whatGPT3.css';
+import {Feature} from '../../components';
 
 const whatGPT3 = () => {
   return (
-    <div>whatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
