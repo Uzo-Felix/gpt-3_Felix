0863bc4830d1b45030d8598b60ded2893d991aa5
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-01-31 17:16:40 +0100

    feat:call to action jsx

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Request early access to get started</p> 
+        <h3>Register today & start exploring the endless possibilites.</h3>
+      </div>
+      <div className='gpt3__cta-btn'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
