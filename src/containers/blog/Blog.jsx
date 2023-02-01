c5db8af7260970884438f91e9d49f426b505849a
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-02-01 16:31:16 +0100

    feat: blog container styling

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './blog.css';
+import {blog01, blog02, blog03, blog04, blog05} from './import';
+import {Article} from '../../components'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id="blog">
+      <div className='gpt3__blog-heading'>
+        <h1 className="gradient__text">A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          < Article imgUrl={blog01} date="sep 26, 2021" title="GPT-3 and Open AI is the future. Let us explor how it is?"/>
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          < Article imgUrl={blog02} date="sep 26, 2021" title="GPT-3 and Open AI is the future. Let us explor how it is?"/>
+          < Article imgUrl={blog03} date="sep 26, 2021" title="GPT-3 and Open AI is the future. Let us explor how it is?"/>
+          < Article imgUrl={blog04} date="sep 26, 2021" title="GPT-3 and Open AI is the future. Let us explor how it is?"/>
+          < Article imgUrl={blog05} date="sep 26, 2021" title="GPT-3 and Open AI is the future. Let us explor how it is?"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
